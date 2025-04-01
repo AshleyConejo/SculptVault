af1b67b4c7d86f3675cd0b273e0dd67799a12094
Ashley <ashley.conejomolina@ontariotechu.net>
2025-03-31 23:57:19 -0400

    Fix DB init for Railway deployment

--- DB-FINAL/website/__init__.py
+++ DB-FINAL/website/__init__.py
@@ -2,27 +2,31 @@ from flask import Flask
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
 import bcrypt
-
+import os
 
 mysql = MySQL()
 
 def application():
     app = Flask(__name__)
+
+    # Railway DB config
     app.config['SECRET_KEY'] = 'ACHLI'
     app.config['MYSQL_HOST'] = 'shinkansen.proxy.rlwy.net'
-    app.config['MYSQL_PORT'] = 48324  
-    app.config['MYSQL_USER'] = 'root'  
-    app.config['MYSQL_PASSWORD'] = 'VbckqelZgKDVuQqCinBlusRoKnuvULcV'  
-    app.config['MYSQL_DB'] = 'railway'  
+    app.config['MYSQL_PORT'] = 48324
+    app.config['MYSQL_USER'] = 'root'
+    app.config['MYSQL_PASSWORD'] = 'VbckqelZgKDVuQqCinBlusRoKnuvULcV'
+    app.config['MYSQL_DB'] = 'railway'
 
+    # Initialize MySQL
+    mysql.init_app(app)
+
+    # Register Blueprints
     from .views import views
     from .auth import auth
-    from .admin import admin 
+    from .admin import admin
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
     app.register_blueprint(admin, url_prefix='/')
-
-    mysql.init_app(app)
 
     return app
