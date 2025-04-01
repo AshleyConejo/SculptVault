a80bc3fd76e4d31cebab9a1c69ead9295e30f8f1
Ashley <ashley.conejomolina@ontariotechu.net>
2025-03-31 23:42:58 -0400

    Fix import path for website package

--- DB-FINAL/main.py
+++ DB-FINAL/main.py
@@ -1,8 +1,13 @@
+import sys
+import os
+
+# Add the current directory to the module search path
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from website import application
-from website.views import views 
+from website.views import views
 
 app = application()
-
 
 if __name__ == '__main__':
     app.run(debug=True)
